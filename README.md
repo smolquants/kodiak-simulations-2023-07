1bf386d35603c5dc34bb23a1995a3440ff0ae753
fmrmf <m@fmrmf.xyz>
2023-08-20 21:56:47 -0400

    add backtester script

--- README.md
+++ README.md
@@ -29,3 +29,38 @@ Then launch [`ape-notebook`](https://github.com/ApeWorX/ape-notebook)
 ```sh
 (kodiak-simulations-2023-07) $ ape notebook
 ```
+
+## Scripts
+
+Scripts using backtester contracts rely on [`backtest-ape`](https://github.com/smolquants/backtest-ape) and
+[`ape-foundry`](https://github.com/ApeWorX/ape-foundry) mainnet-fork functionality. These produce backtest results
+for different tick range strategies.
+
+Compile the needed contracts
+
+```sh
+(kodiak-simulations-2023-07) ape compile --size
+```
+
+Then run the backtest script e.g. for fee 0 amounts
+
+```sh
+(kodiak-simulations-2023-07) ape run backtester
+INFO: Starting 'anvil' process.
+You are connected to provider network ethereum:mainnet-fork:foundry.
+Runner type (UniswapV3LPFixedWidthFee0Runner, UniswapV3LPFixedWidthFee1Runner): UniswapV3LPFixedWidthFee0Runner
+Runner kwarg (ref_addrs) [{}]: {"pool": "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640", "manager": "0xC36442b4a4522E871399CD717aBDD847Ab11FE88"}
+Runner kwarg (acc_addr) defaults to None. Do you want to input a value? [y/N]: N
+Runner kwarg (tick_lower) [0]: 200280
+Runner kwarg (tick_upper) [0]: 207240
+Runner kwarg (amount_weth) [0]: 67000000000000000000
+Runner kwarg (amount_token) [0]: 34427240000
+Runner kwarg (tick_width) [0]: 6960
+Runner kwarg (blocks_between_rebalance) [0]: 50400
+Runner kwarg (block_rebalance_last) [0]: 16219692
+Method (backtest, replay, forwardtest): backtest
+Start block number: 16219692
+Stop block number [-1]:
+Step size [1]:
+Setting up runner ...
+```
---
+++ scripts/backtester.py
@@ -0,0 +1,77 @@
+from ast import literal_eval
+
+import click
+from ape import networks
+from typing_inspect import get_origin
+
+import kodiak_simulations_2023_07
+
+
+def main():
+    """
+    Main backtester script.
+    """
+    # echo provider setup
+    ecosystem_name = networks.provider.network.ecosystem.name
+    network_name = networks.provider.network.name
+    provider_name = networks.provider.name
+    connection_name = f"{ecosystem_name}:{network_name}:{provider_name}"
+    click.echo(f"You are connected to provider network {connection_name}.")
+
+    # fail if not mainnet-fork
+    if network_name != "mainnet-fork":
+        raise ValueError("not connected to mainnet-fork.")
+
+    # prompt user which backtest runner to use
+    runner_cls_name = click.prompt(
+        "Runner type", type=click.Choice(kodiak_simulations_2023_07.__all__, case_sensitive=False)
+    )
+    runner_cls = getattr(kodiak_simulations_2023_07, runner_cls_name)
+
+    # prompt user for fields on runner to init with
+    kwargs = {}
+    for name, field in runner_cls.__fields__.items():
+        # default to str if not base type
+        type_origin = get_origin(field.annotation)
+        type_ = field.annotation if type_origin is None else str
+
+        # confirm prompt if Optional
+        if field.default is None:
+            if not click.confirm(
+                f"Runner kwarg ({name}) defaults to None. Do you want to input a value?"
+            ):
+                kwargs[name] = field.default
+                continue
+
+        value = click.prompt(
+            f"Runner kwarg ({name})", default=field.default, type=type_
+        )
+
+        # parse field value from str if not base type
+        if type_origin is not None:
+            value = literal_eval(value)
+
+        kwargs[name] = value
+
+    # setup runner
+    runner = runner_cls(**kwargs)
+
+    # prompt user for choice of method: backtest, replay, or forwardtest
+    method_name = click.prompt(
+        "Method",
+        type=click.Choice(["backtest", "replay", "forwardtest"], case_sensitive=False),
+    )
+
+    # run backtest
+    start = click.prompt("Start block number", type=int)
+    stop = click.prompt("Stop block number", type=int, default=-1)
+    step = click.prompt("Step size", type=int, default=1)
+    path = f"scripts/results/{runner_cls_name}_{method_name}_{start}_{stop}_{step}.csv"
+    if stop < 0:
+        stop = None
+
+    args = [path, start, stop]
+    if method_name != "replay":
+        args.append(step)
+
+    getattr(runner, method_name)(*args)
